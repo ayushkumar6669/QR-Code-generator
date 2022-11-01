a34331075eb35cd99fdb9bb96d586392cf0e1f5c
Ayush Kumar <raj6669ayush@gmail.com>
2022-11-01 14:50:56 +0530

    remove active if input empty

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const wrapper= document.querySelector(".wrapper"),
+const wrapper = document.querySelector(".wrapper"),
 qrInput = wrapper.querySelector(".form input"),
-generateBtn = wrapper.querySelector(".form button");
+generateBtn = wrapper.querySelector(".form button"),
 qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
 
-generateBtn.addEventListener("click",()=>{
+generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value.trim();
-    if(!qrValue || preValue === qrValue) return; //if input empty then return
-    //fetching QR code using qrserver api
-    qrImg.src=` https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
-    wrapper.classList.add("active");
-}
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
