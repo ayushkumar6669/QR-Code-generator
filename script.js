655b38a56cf837455804e9adc9165d77b7077566
Ayush Kumar <raj6669ayush@gmail.com>
2022-11-01 14:25:25 +0530

    fetching QR code using qrserver api

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const wrapper= document.querySelector(".wrapper"),
 qrInput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button");
+qrImg = wrapper.querySelector(".qr-code img");
 
 generateBtn.addEventListener("click",()=>{
     let qrValue = qrInput.value.trim();
-    if(!qrValue || preValue === qrValue) return;
+    if(!qrValue || preValue === qrValue) return; //if input empty then return
+    //fetching QR code using qrserver api
+    qrImg.src=` https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
     wrapper.classList.add("active");
 }
